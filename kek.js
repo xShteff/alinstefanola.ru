1a795dc9a9e8416dcb095818ebed46c44db731b4
Allen <allenmcpotter@outlook.com>
2016-02-06 23:27:25 +0100

    attempt to fix

--- kek.js
+++ kek.js
@@ -1,7 +1,13 @@
-var asd = $('<iframe></iframe>').attr({
-	'width' : '140px',
-	'height' : '140px',
-	'src' : 'https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1',
-	'frameborder' : '0'
-}).css('padding-top', '7px');
-WestUi.setAvatar(asd);
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.textContent = '(' + (function () {
+	var asd = $('<iframe></iframe>').attr({
+		'width' : '140px',
+		'height' : '140px',
+		'src' : 'https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1',
+		'frameborder' : '0'
+	}).css('padding-top', '7px');
+	WestUi.setAvatar(asd);
+}).toString() + ')()';
+if (location.hostname.indexOf('the-west') >= 0 && location.pathname === '/game.php')
+    document.head.appendChild(script);
