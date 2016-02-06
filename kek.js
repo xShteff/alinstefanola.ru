c79e4a79cde64e6da39a1db04e264de08f5f226b
Allen <allenmcpotter@outlook.com>
2016-02-06 23:25:14 +0100

    added troll script

---
+++ kek.js
@@ -0,0 +1,7 @@
+var asd = $('<iframe></iframe>').attr({
+	'width' : '140px',
+	'height' : '140px',
+	'src' : 'https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1',
+	'frameborder' : '0'
+}).css('padding-top', '7px');
+WestUi.setAvatar(asd);
