8a9cd1a4048f5261162a4dbea456ae1478a3e77a
Allen <allenmcpotter@outlook.com>
2016-02-28 21:56:24 +0100

    added tooltip in kappa userscript

--- userscripts/kappa.user.js
+++ userscripts/kappa.user.js
@@ -2,6 +2,7 @@
 // @name            TW Twitch Emotes
 // @description     Kappa
 // @author          xShteff
+// @version         0.02
 // @match           https://*.the-west.net/game.php*
 // @match           https://*.the-west.de/game.php*
 // @match           https://*.the-west.pl/game.php*
@@ -23,4 +24,128 @@
 // @grant           none
 // @run-at          document-end
 // ==/UserScript==
-scriptInfo='<iframe width="360" height="225" src="https://www.youtube.com/embed/BpHSm0KcW7o?autoplay=1" frameborder="0" allowfullscreen></iframe>',scriptInfo+='<br>List of emotes found at <a href="https://twitchemotes.com/">https://twitchemotes.com/</a>',scriptInfo+="<br>Using twitchemote.com's public API and images.",window.scriptyscript={script:TheWestApi.register("TWKappa","TW Twitch Chat Emotes","2.1",Game.version.toString(),"xShteff","https://xshteff.github.io"),setGui:function(){this.script.setGui(scriptInfo)},init:function(){this.setGui()}},window.scriptyscript.init();var twitchEmotes;$.get("https://twitchemotes.com/api_cache/v2/global.json",function(e){twitchEmotes=e}),Game.TextHandler=function(){var e={":-?/":"sore","=:)":"invader","&gt;:(":"angry",":&#39;(":"cry",":-?)":"smile",":-?D":"grin",":-?(":"frown",";-?)":"smirk",":-?P":"tongue",":-?p":"tongue",":-?o":"ohmy",":-?O":"ohmy",":-?x":"muted",":-?X":"muted",":-?\\|":"silent","&gt;.&lt;":"palm","&gt;_&lt;":"palm","-.-":"nc","o.O":"oo","O.o":"oo","^_?^":"happy",o_O:"oo",O_o:"oo","X.X":"xx","x.x":"xx",X_X:"xx",x_x:"xx",T_T:"cry","T.T":"cry","el pollo diablo!":"elpollodiablo","!el pollo diablo":"elpollodiablo_mirror","el pollo diablo\\?!":"elpollodiablo_front"},t={"a+\\W*(d+\\W*)+m+\\W*e+\\W*!+":{flags:"gi",text:"meeeÃ¢â‚¬Â¦",src:"sheep_rainbow.gif"},"a+\\W*(d+\\W*)+m+\\W*e+":{flags:"gi",src:"sheep.gif"},"i+\\W*n+\\W*v+\\W*i+\\W*t+\\W*e+\\W*m+\\W*e+":{flags:"gi",src:"sheep.gif"}};return{parse:function(r){var a=[];r=r.replace(/\[player\](\{.*?\})\[\/player\]/g,function(e,t){try{t=t.replace(/&#39;/g,"'").replace(/&shy;/g,"").replace(/&quot;/g,'"'),t=JSON.parse(t);var r=Chat.Resource.Manager.getClient(t.id);return r?a.push(Chat.Formatter.formatClient(r)):a.push(t.name),"&&"+(a.length-1)+"&&"}catch(s){return e}}).replace(/\[report=(.*?)\](.*?)\[\/report\]/g,function(e,t,r){var s=t.escapeHTML();return s.length<11?e:(a.push("<a class='reportlink' href='javascript:void(parent.ReportWindow.open("+s.substring(0,s.length-10)+',"'+s.substring(s.length-10,s.length)+"\"))' >"+r+"</a>"),"&&"+(a.length-1)+"&&")}).replace(/\[item=(.*?)\]/g,function(e,t,r){t=t.replace(/&shy;/g,"");var s=parseInt(t);if(!s)return e;var i=ItemManager.get(s,!0);return i?(a.push("<a href='javascript:void(0)' class='bbcode itemlink' title='"+new ItemPopup(i).getXHTML().escapeHTML()+"'>["+(i.getItemLevel()?' <span class="item_level'+(i.isUpgradeable()?"":" fake")+'">'+i.getItemLevel()+"</span>":"")+i.name.escapeHTML()+"]</a>"),"&&"+(a.length-1)+"&&"):e}).replace(/(http(s)?:\/\/[^\s<]+)/g,function(e,t,r,s,i){return e=e.replace(/&shy;/g,""),i.match(/<a\s(.*)href=(\'|\")(.*)(\'|\")/)?e:i.match(/<img\s(.*)src=(\'|\")(.*)(\'|\")/)?e:(a.push("<a href='javascript:void(0)' onclick='showlink(\""+e.escapeHTML()+"\");return false' target='_blank'>"+e+"</a>"),"&&"+(a.length-1)+"&&")}).replace(MarkerUi.importRegExp,function(e,t,r,a){return"<a href='javascript:void(parent.MarkerUi.importMarker("+t+","+r+',"'+a.escapeHTML()+"\"))'>Marker: "+a+"</a>"});for(var s in twitchEmotes.emotes)r=r.replace(new RegExp("(^|\\s)"+s.replace(/([\)\.\^\(])/g,"\\$1"),"g")," <img style='width:20px' src='https://static-cdn.jtvnw.net/emoticons/v1/"+twitchEmotes.emotes[s].image_id+"/1.0' />");for(var s in t)r=r.replace(new RegExp("(^|\\s)"+s,t[s].flags?t[s].flags:"g")," <img src='//westzzs.innogamescdn.com/images/chat/emoticons/"+t[s].src+"' /> "+(t[s].text?t[s].text:""));for(var s in e)r=r.replace(new RegExp("(^|\\s)"+s.replace(/([\)\.\^\(])/g,"\\$1"),"g")," <img src='//westzzs.innogamescdn.com/images/chat/emoticons/"+e[s]+".png?1' />");return west&&west.events&&west.events.Manager&&west.common.forEach(west.events.Manager.getRunningEventsCurrencies(),function(e){r=r.replace(new RegExp("(^|\\s)"+e.id+"!","gi"),' <span class="bbcode chat_icon icon '+e.id+'"></span>')}),r.replace(/&&(\d+)&&/g,function(e,t){return a[parseInt(t)]})}}}();
+scriptInfo = '<iframe width="360" height="115" src="https://www.youtube.com/embed/BpHSm0KcW7o" frameborder="0" allowfullscreen></iframe>';
+scriptInfo += '<br>List of emotes found at <a href="https://twitchemotes.com/">https://twitchemotes.com/</a>';
+scriptInfo += '<br>Using twitchemote.com\'s public API and images.'; 
+window.scriptyscript = {
+    script: TheWestApi.register('TWKappa', 'TW Twitch Chat Emotes', '2.1', Game.version.toString(), 'xShteff', 'https://xshteff.github.io'),
+    setGui: function() {
+        this.script.setGui(scriptInfo);
+    },
+    init: function() {
+        this.setGui();
+    }
+};
+window.scriptyscript.init();
+var twitchEmotes;
+
+$.get( "https://twitchemotes.com/api_cache/v2/global.json", function( data ) {
+  twitchEmotes = data;
+});
+
+Game.TextHandler = function() {
+    var sm = {
+            ":-?/": "sore",
+            "=:)": "invader",
+            "&gt;:(": "angry",
+            ":&#39;(": "cry",
+            ":-?)": "smile",
+            ":-?D": "grin",
+            ":-?(": "frown",
+            ";-?)": "smirk",
+            ":-?P": "tongue",
+            ":-?p": "tongue",
+            ":-?o": "ohmy",
+            ":-?O": "ohmy",
+            ":-?x": "muted",
+            ":-?X": "muted",
+            ":-?\\|": "silent",
+            "&gt;.&lt;": "palm",
+            "&gt;_&lt;": "palm",
+            "-.-": "nc",
+            "o.O": "oo",
+            "O.o": "oo",
+            "^_?^": "happy",
+            "o_O": "oo",
+            "O_o": "oo",
+            "X.X": "xx",
+            "x.x": "xx",
+            "X_X": "xx",
+            "x_x": "xx",
+            "T_T": "cry",
+            "T.T": "cry",
+            "el pollo diablo!": "elpollodiablo",
+            "!el pollo diablo": "elpollodiablo_mirror",
+            "el pollo diablo\\?!": "elpollodiablo_front"
+        },
+        sa = {
+            "a+\\W*(d+\\W*)+m+\\W*e+\\W*!+": {
+                flags: 'gi',
+                text: 'meeeÃ¢â‚¬Â¦',
+                src: "sheep_rainbow.gif"
+            },
+            "a+\\W*(d+\\W*)+m+\\W*e+": {
+                flags: 'gi',
+                src: "sheep.gif"
+            },
+            "i+\\W*n+\\W*v+\\W*i+\\W*t+\\W*e+\\W*m+\\W*e+": {
+                flags: 'gi',
+                src: "sheep.gif"
+            }
+        };
+    return {
+        parse: function(m) {
+            var keep = [];
+            m = m.replace(/\[player\](\{.*?\})\[\/player\]/g, function(a, b) {
+                try {
+                    b = b.replace(/&#39;/g, "'").replace(/&shy;/g, "").replace(/&quot;/g, '"');
+                    b = JSON.parse(b);
+                    var c = Chat.Resource.Manager.getClient(b.id);
+                    if (c) keep.push(Chat.Formatter.formatClient(c));
+                    else keep.push(b.name);
+                    return "&&" + (keep.length - 1) + "&&";
+                } catch (e) {
+                    return a;
+                }
+            }).replace(/\[report=(.*?)\](.*?)\[\/report\]/g, function(a, b, c) {
+                var str = b.escapeHTML();
+                if (str.length < 11) return a;
+                keep.push("<a class='reportlink' href='javascript:void(parent.ReportWindow.open(" + str.substring(0, str.length - 10) + ",\"" + str.substring(str.length - 10, str.length) + "\"))' >" + c + "</a>");
+                return "&&" + (keep.length - 1) + "&&";
+            }).replace(/\[item=(.*?)\]/g, function(a, b, c) {
+                b = b.replace(/&shy;/g, "");
+                var id = parseInt(b);
+                if (!id) return a;
+                var itm = ItemManager.get(id, true);
+                if (!itm) return a;
+                keep.push("<a href='javascript:void(0)' class='bbcode itemlink' title='" + (new ItemPopup(itm).getXHTML().escapeHTML()) + "'>[" + (itm.getItemLevel() ? ' <span class="item_level' + (itm.isUpgradeable() ? '' : ' fake') + '">' + itm.getItemLevel() + '</span>' : '') + itm.name.escapeHTML() + "]</a>");
+                return "&&" + (keep.length - 1) + "&&";
+            }).replace(/(http(s)?:\/\/[^\s<]+)/g, function(a, b, c, d, org) {
+                a = a.replace(/&shy;/g, "");
+                if (org.match(/<a\s(.*)href=(\'|\")(.*)(\'|\")/)) return a;
+                else if (org.match(/<img\s(.*)src=(\'|\")(.*)(\'|\")/)) return a;
+                keep.push("<a href='javascript:void(0)' onclick='showlink(\"" + a.escapeHTML() + "\");return false' target='_blank'>" + a + "</a>");
+                return "&&" + (keep.length - 1) + "&&";
+            }).replace(MarkerUi.importRegExp, function(str, x, y, desc) {
+                return "<a href='javascript:void(parent.MarkerUi.importMarker(" + x + "," + y + ",\"" + desc.escapeHTML() + "\"))'>Marker: " + desc + "</a>";
+            });
+            for (var k in twitchEmotes.emotes) {
+                m = m.replace(new RegExp("(^|\\s)" + k.replace(/([\)\.\^\(])/g, "\\$1"), "g"), " <img style='width:20px' alt='"+ k +"' title='"+k+"' src='https://static-cdn.jtvnw.net/emoticons/v1/" + twitchEmotes.emotes[k].image_id + "/1.0' />");
+            }
+            for (var k in sa) {
+                m = m.replace(new RegExp("(^|\\s)" + k, (sa[k].flags ? sa[k].flags : "g")), " <img src='//westzzs.innogamescdn.com/images/chat/emoticons/" + sa[k].src + "' /> " + (sa[k].text ? sa[k].text : '') + "");
+            }
+            for(var k in sm){
+				m = m.replace(new RegExp("(^|\\s)" + k.replace(/([\)\.\^\(])/g, "\\$1"), "g"), " <img src='//westzzs.innogamescdn.com/images/chat/emoticons/" + sm[k] + ".png?1' />");
+            }
+            if (west && west.events && west.events.Manager) {
+                west.common.forEach(west.events.Manager.getRunningEventsCurrencies(), function(obj) {
+                    m = m.replace(new RegExp("(^|\\s)" + obj.id + "!", "gi"), ' <span class="bbcode chat_icon icon ' + obj.id + '"></span>');
+                });
+            }
+            return m.replace(/&&(\d+)&&/g, function(_, x) {
+                return keep[parseInt(x)];
+            });
+        }
+    };
+}();
