cedf7756d6ebecc31028947dbbbe28fb7f017293
Alin-Stefan Olaru <allenmcpotter@outlook.com>
2017-01-11 19:21:01 +0100

    Another update to TWKappa because I'm a derp and forgot to bump version number...

--- userscripts/kappa.user.js
+++ userscripts/kappa.user.js
@@ -33,7 +33,7 @@ var TWKappa = {
         video_height: 190
     },
     VersionControl: {
-        version: 0.11,
+        version: 0.12,
         isOutdated: function() {
             return TWKappa.Emotes.Extra.storage.latestVersion > TWKappa.VersionControl.version;
         },
