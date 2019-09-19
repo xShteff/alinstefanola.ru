649ca08752d08657ca6321a55e155881713b8334
Alin Stefan Olaru <alinstefanolaru@gmail.com>
2019-09-19 21:14:46 +0200

    Added home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { NgZorroAntdModule, NZ_I18N, en_US } from "ng-zorro-antd";
+import { HomeComponent } from './components/main/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent],
   imports: [BrowserModule, AppRoutingModule, NgZorroAntdModule],
   providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
