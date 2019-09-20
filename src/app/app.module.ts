31f3bf092d771048664a959f9ae883def5db2bbf
Alin Stefan Olaru <alinstefanolaru@gmail.com>
2019-09-20 16:25:54 +0200

    Fixed linting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { NgZorroAntdModule, NZ_I18N, en_US } from "ng-zorro-antd";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgZorroAntdModule, NZ_I18N, en_US } from 'ng-zorro-antd';
 import { HomeComponent } from './components/main/home/home.component';
 import { ParallaxIntroComponent } from './components/main/parallax-intro/parallax-intro.component';
 
